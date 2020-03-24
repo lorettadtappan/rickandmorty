610660f93840b66b2e6c8307c7aa144dd82ae660
AndroidDoctorr <gbartthenerd@gmail.com>
2020-03-24 19:40:51 -0400

    added morty class

---
+++ RickAndMortyGame/Mortys/IMorty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RickAndMortyGame.Mortys
+{
+    public interface IMorty
+    {
+        int Health { get; }
+        void Scream();
+        void Hurt(int damage);
+        int Attack();
+    }
+}
---
+++ RickAndMortyGame/Mortys/WerewolfMorty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RickAndMortyGame.Mortys
+{
+    public class WerewolfMorty : IMorty
+    {
+        Random rand = new Random();
+        public int Health { get; set; } = 25;
+        public void Scream()
+        {
+            Console.WriteLine("RRAAAWWWWWOOOOOOOoooooOOOOooooowwww");
+        }
+        public void Hurt(int damage)
+        {
+            Health -= damage;
+            Scream();
+        }
+        public int Attack()
+        {
+            return rand.Next(5, 11);
+        }
+    }
+}
