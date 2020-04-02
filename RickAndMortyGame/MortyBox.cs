06ec611fb6a834c0e1d884c4012ebf096107424b
Todd Newland <tnewland08@gmail.com>
2020-04-02 19:25:48 -0400

    added MortyBox

---
+++ RickAndMortyGame/MortyBox.cs
@@ -0,0 +1,34 @@
+﻿using RickAndMortyGame.Mortys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RickAndMortyGame
+{
+    class MortyBox
+    {
+        private List<IMorty> mortys = new List<IMorty>();
+
+        public List<IMorty> GetAllMortys()
+        {
+            return mortys;
+        }
+
+        public void CollectMorty(IMorty morty)
+        {
+            mortys.Add(morty);
+        }
+
+        public void DumpMorty(IMorty morty)
+        {
+            bool isInList = mortys.Contains(morty);
+
+            if (isInList)
+            {
+                mortys.Remove(morty);
+            }
+        }
+    }
+}
