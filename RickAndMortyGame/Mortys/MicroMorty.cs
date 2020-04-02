21c4f85aff73910e1ecf21010ac4d4d9df64e5cf
Todd Newland <tnewland08@gmail.com>
2020-04-02 18:35:29 -0400

    added MicroMorty

---
+++ RickAndMortyGame/Mortys/MicroMorty.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RickAndMortyGame.Mortys
+{
+    class MicroMorty
+    {
+        public int Health { get; set; }
+
+        public MicroMorty()
+        {
+            Health = 50;
+        }
+
+        public Attack Attack() 
+        {
+            int maxDamage = 30;
+            int minDamage = 5;
+            Attack pileDrive = new Attack(minDamage, maxDamage, RickAndMortyGame.Attack.DamageType.Bludgeoning);
+            return pileDrive;
+        }
+
+        void Scream()
+        {
+            Console.WriteLine("aaaaaahhhhhhh!!!!");
+        }
+
+        void Hurt(Attack attack)
+        {
+            Health -= attack.Damage;
+        }
+    }
+}
